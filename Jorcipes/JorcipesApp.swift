156f3fbfda5a1bbff2b7b2a611b3ceb2ea33913b
Jordi Kitto <jordikitto@icloud.com>
2026-02-28 10:11:31 +1100

    Initial Commit

---
+++ Jorcipes/JorcipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JorcipesApp.swift
+//  Jorcipes
+//
+//  Created by Jordi Kitto on 28/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct JorcipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JorcipesTests/JorcipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  JorcipesTests.swift
+//  JorcipesTests
+//
+//  Created by Jordi Kitto on 28/2/2026.
+//
+
+import Testing
+@testable import Jorcipes
+
+struct JorcipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
